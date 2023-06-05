551555e574826ae8f06cd8fcd731343afe94a4ba
giuliabruno05 <giuliabruno48@gmail.com>
2023-06-05 16:17:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <select>
+      <option value="alien">Alien</option>
+    </select>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+
+main {
+  background-color: $main-color;
+  min-height: 600px;
+  padding: 20px;
+
+  select {
+    width: 11%;
+    background-color: $white;
+    padding: 5px;
+    border-radius: 3px;
+    border-color: lightgray;
+    border: hidden;
+  }
+
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+
+<template>
+    <header>
+        <img class="logo-header" src="/Yugioh_anime_logo.webp" alt="">
+        <h2>Yu-Gi-Oh Api</h2>
+    </header>
+</template>
+
+
+
+<style lang="scss" scoped>
+header {
+    height: 50px;
+    display: flex;
+    align-items: center;
+
+    .logo-header {
+        width: 80px;
+        margin-right: 10px;
+    }
+
+    h2 {
+        font-weight: 300;
+    }
+
+}
+</style>
